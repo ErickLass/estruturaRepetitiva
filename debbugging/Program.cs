c00f5db868f17b2b6e5b6fdcb98559a61807d182
marco <marco@DESKTOP-CJTTCG2>
2023-08-30 11:08:22 -0300

    Adicionar arquivos de projeto.

---
+++ debbugging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Programa para fazer raiz quadrada");
+        Console.Write("Digite um numero: ");
+        double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (x >= 0)
+        {
+            double raiz = Math.Sqrt(x); //raiz quadrada
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.Write("Digite outro numero: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        Console.WriteLine("Numero negativo!");
+    }
+}
